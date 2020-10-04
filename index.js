7027fe5ab144bd48fb585a109da2b186382a4db7
sdonohue0918 <sdonohue0918@gmail.com>
2020-10-04 12:07:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+let element = document.createElement('h1');
+document.getElementById('main').removeAttribute('id');
+document.body.appendChild(element);
+element.innerHTML = "Dingus LaFleur is the champion!";
+
+element.id = 'victory';
+
+
+
+
+
+
+
+
+
