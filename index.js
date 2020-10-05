e9e2029115b039b2983dcfd57de3ae7afdd54e99
nah1222 <nah1222@yahoo.com>
2020-10-04 22:10:49 -0400

    Done.

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
 newHeader.innerText = "Nick is the champion!";
-newHeader.h1 = "Nick is the champion";
+newHeader.h1 = "Nick is the champion!";
 main.remove('#main')
